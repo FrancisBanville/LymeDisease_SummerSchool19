6d0db9066c8d8d1a1707470d1233bd7189539a8f
Miléna <milena_aragon@yahoo.ca>
2019-05-04 10:00:23 -0400

    ajout données pop miléna census

---
+++ travail1.R
@@ -0,0 +1,13 @@
+# Package data "noncensus"
+
+library (noncensus)
+data(package = "noncensus")
+data("states")
+data("counties")
+
+library (totalcensus)
+data(package = "totalcensus")
+
+library (census) 
+
+
